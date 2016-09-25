23d89f4fe5fe5658f6c858cf2b620200dd94dc14
@CAYdenberg <ydenberg@gmail.com>
2016-09-25 14:22:07 -0600

    Add 404 handling

--- app.js
+++ app.js
@@ -53,11 +53,18 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  switch(res.status) {
+    case 404:
+      res.render('error', {
+        message: '404. The page you are looking for cannot be found.'
+      });
+      break;
+
+    default:
+      res.render('error', {
+        message: 'Internal server error. The page cannot be rendered.'
+      })
+  }
 });
 
 
