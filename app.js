0d5684ff95125410f0d544f2048f4f028b22e522
@CAYdenberg <ydenberg@gmail.com>
2017-01-09 20:02:50 -0700

    Basic keystone integration working

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').load();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,6 +12,9 @@ var hbs = require('express-hbs');
 
 var routes = require('./routes/index');
 
+var session = require('express-session');
+var keystone = require('keystone');
+
 var app = express();
 
 // uncomment after placing your favicon in /public
@@ -15,7 +22,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
@@ -23,47 +30,31 @@ app.engine('hbs', hbs.express3());
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(session({
+  secret: process.env.COOKIE_SECRET
+}));
+
 app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+keystone.init({
+  'env': process.env.NODE_ENV,
+  'mongo': process.env.MONGO_URI,
+	'url': process.env.URL,
+
+  'name': 'ExchangeJS',
+
+  'auto update': true,
+  'session': true,
+  'session store': 'mongo',
+  'auth': true,
+  'user model': 'User'
 });
+// Load your project's Models
+keystone.import('models');
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (process.env.NODE_ENV === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    switch(err.status) {
-        case 404:
-            res.status(404);
-            res.render('error', {
-              message: '404. The page you are looking for cannot be found.'
-            });
-            break;
-
-        default:
-            res.status(500);
-            res.render('error', {
-              message: 'Internal server error. The page cannot be rendered.'
-            })
-    }
-});
+keystone.set('app', app);
+app.use('/keystone', require('keystone/admin/server/app/createStaticRouter.js')(keystone));
+app.use('/keystone', require('keystone/admin/server/app/createDynamicRouter.js')(keystone));
 
 
-module.exports = app;
+keystone.start();
