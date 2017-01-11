4601448e9094f9dfb5c2ec40f821a6439cb3eb16
@CAYdenberg <ydenberg@gmail.com>
2017-01-11 09:40:07 -0700

    Enforce 2 spaces on indent

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ require('dotenv').load();
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -38,7 +37,7 @@ app.use(session({
 keystone.init({
   'env': process.env.NODE_ENV,
   'mongo': process.env.MONGO_URI,
-	'url': process.env.URL,
+  'url': process.env.URL,
 
   'name': 'ExchangeJS',
 
@@ -58,6 +57,45 @@ app.use('/', routes(keystone));
 app.use('/keystone', require('keystone/admin/server/app/createStaticRouter.js')(keystone));
 app.use('/keystone', require('keystone/admin/server/app/createDynamicRouter.js')(keystone));
 
-// add 404 routes
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (process.env.NODE_ENV === 'development') {
+  app.use(function(err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res) {
+  switch(err.status) {
+    case 404:
+      res.status(404);
+      res.render('error', {
+        message: '404. The page you are looking for cannot be found.'
+      });
+      break;
+
+    default:
+        res.status(500);
+        res.render('error', {
+          message: 'Internal server error. The page cannot be rendered.'
+        })
+  }
+});
+
 
 keystone.start();
