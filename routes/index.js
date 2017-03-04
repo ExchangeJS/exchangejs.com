b413d4e1144d13402562c3b192d79f337ce9204d
@CAYdenberg <ydenberg@gmail.com>
2017-03-04 14:35:07 -0700

    Helpers view

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,39 @@
 var express = require('express');
 
 module.exports = (keystone) => {
-  let router = express.Router();
+	let router = express.Router();
 
-  /* GET home page. */
-  router.get('/', function(req, res) {
-    res.render('index', {title: 'Exchange.js'});
-  });
+	/* GET home page. */
+	router.get('/', function(req, res) {
+		res.render('index', {title: 'Exchange.js'});
+	});
 
-  router.get('/:page', function(req, res, next) {
-    keystone.list('Page').model.findOne({
-      state: 'published',
-      slug: req.params.page
-    }).exec(function(err, result) {
-      if (!result) {
-        return next();
-      }
-      res.render('page', {
-        title: result.title,
-        content: result.content.html
-      });
-    });
-  });
+	router.get('/talks', (req, res) => {
+		keystone.list('Talk').model.find()
+			.sort('-presentedOn')
+			.populate('speaker')
+			.exec((err, talks) => {
+				res.render('talks', {
+					title: 'Talks | Exchange.js',
+					talks: talks
+				});
+			});
+	});
 
-  return router;
+	router.get('/:page', function(req, res, next) {
+		keystone.list('Page').model.findOne({
+			state: 'published',
+			slug: req.params.page
+		}).exec(function(err, result) {
+			if (!result) {
+				return next();
+			}
+			res.render('page', {
+				title: result.title,
+				content: result.content.html
+			});
+		});
+	});
+
+	return router;
 }
