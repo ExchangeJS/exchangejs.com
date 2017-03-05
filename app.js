e095f8f977dbbc4eea5f8f75038d7ad44e129bf7
@CAYdenberg <ydenberg@gmail.com>
2017-03-04 19:45:00 -0700

    Markup for talks

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var routes = require('./routes/index');
 
 var session = require('express-session');
 var keystone = require('keystone');
+var moment = require('moment');
 
 var app = express();
 
@@ -29,23 +30,63 @@ app.engine('hbs', hbs.express3());
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+// ### Date Helper
+// A port of the Ghost Date formatter similar to the keystonejs - jade interface
+//
+//
+// *Usage example:*
+// `{{date format='MM YYYY}}`
+// `{{date publishedDate format='MM YYYY'`
+//
+// Returns a string formatted date
+// By default if no date passed into helper than then a current-timestamp is used
+//
+// Options is the formatting and context check this.publishedDate
+// If it exists then it is formated, otherwise current timestamp returned
+hbs.registerHelper('date', function(context, options) {
+	if (!options && context.hasOwnProperty('hash')) {
+		options = context;
+		context = undefined;
+
+		if (this.publishedDate) {
+			context = this.publishedDate;
+		}
+	}
+
+	// ensure that context is undefined, not null, as that can cause errors
+	context = context === null ? undefined : context;
+
+	var f = options.hash.format || 'MMM Do, YYYY',
+		timeago = options.hash.timeago,
+		date;
+
+	// if context is undefined and given to moment then current timestamp is given
+	// nice if you just want the current year to define in a tmpl
+	if (timeago) {
+		date = moment(context).fromNow();
+	} else {
+		date = moment(context).format(f);
+	}
+	return date;
+});
+
 app.use(session({
-  secret: process.env.COOKIE_SECRET
+	secret: process.env.COOKIE_SECRET
 }));
 
 
 keystone.init({
-  'env': process.env.NODE_ENV,
-  'mongo': process.env.MONGO_URI,
-  'url': process.env.URL,
+	'env': process.env.NODE_ENV,
+	'mongo': process.env.MONGO_URI,
+	'url': process.env.URL,
 
-  'name': 'ExchangeJS',
+	'name': 'ExchangeJS',
 
-  'auto update': true,
-  'session': true,
-  'session store': 'mongo',
-  'auth': true,
-  'user model': 'User'
+	'auto update': true,
+	'session': true,
+	'session store': 'mongo',
+	'auth': true,
+	'user model': 'User'
 });
 // Load your project's Models
 keystone.import('models');
@@ -59,9 +100,9 @@ app.use('/keystone', require('keystone/admin/server/app/createDynamicRouter.js')
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -69,32 +110,33 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (process.env.NODE_ENV === 'development') {
-  app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res) {
-  switch(err.status) {
-    case 404:
-      res.status(404);
-      res.render('error', {
-        message: '404. The page you are looking for cannot be found.'
-      });
-      break;
+	switch(err.status) {
 
-    default:
-        res.status(500);
-        res.render('error', {
-          message: 'Internal server error. The page cannot be rendered.'
-        })
-  }
+	case 404:
+		res.status(404);
+		res.render('error', {
+			message: '404. The page you are looking for cannot be found.'
+		});
+		break;
+
+	default:
+		res.status(500);
+		res.render('error', {
+			message: 'Internal server error. The page cannot be rendered.'
+		})
+	}
 });
 
 
