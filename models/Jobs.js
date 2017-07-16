a7624da0f691d293819e6ae4f8f1df048b8be7e1
Mark Bennett <mark@burmis.ca>
2017-07-16 17:49:15 -0600

    Simplify the job listing.
    I've eliminated extra information and focused on essentials, with the idea that we can just link to the jobs instead.

--- models/Jobs.js
+++ models/Jobs.js
@@ -15,21 +15,13 @@ Job.add({
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	expires_on: { type: Types.Date, initial: true, required: true },
 	title: { type: String, initial: true, required: true },
-	department: { type: String },
 	employment_type: { type: Types.Select, options: 'full-time, part-time, casual, contract', default: 'full-time', required: true },
-	minimum_experience: { type: String },
 	company: { type: String, initial: true, required: true },
 	company_logo: { type: Types.Url },
 	location: { type: String, initial: true, required: true },
-	is_remote: { type: Boolean, label: 'Can you work remotely?' },
-	compensation: { type: String },
-	comp_type: { type: Types.Select, options: 'salary, contract, options', default: 'salary', index: true },
 	summary: { type: Types.Markdown, wysiwyg: true, required: true, initial: true },
-	work_desc: { type: Types.Markdown, wysiwyg: true, required: true, initial: false },
-	requirements: { type: Types.Markdown, wysiwyg: true, required: true, initial: false },
-	why_you_should_apply: { type: Types.Markdown, wysiwyg: true, required: true, initial: false},
-	find_out_more: { type: Types.Url },
-	email_to_apply: { type: Types.Email, required: true, initial: true }
+	url_base: { type: Types.Url, required: true, initial: true },
+	url_to_apply: { type: Types.Url, required: true, initial: true }
 });
 
 Job.defaultColumns = 'title, company, state|20%, expires_on';
